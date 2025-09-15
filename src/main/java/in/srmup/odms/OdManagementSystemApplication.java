521beb51813e6869ba6b13397fb6d63b6e2de186
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-15 18:34:23 +0000

    bootstrap: Initialize Project with Spring Initializr

---
+++ src/main/java/in/srmup/odms/OdManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package in.srmup.odms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/srmup/odms/OdManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package in.srmup.odms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
